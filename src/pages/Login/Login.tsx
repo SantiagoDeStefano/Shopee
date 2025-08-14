f636f4b9278b22cad3b92b0c5aee4c6c04c97a42
Phạm Khôi Nguyên <ngpham.2807@gmail.com>
2025-08-14 17:25:32 +0700

    Register Form validation with React Hook Form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,38 +1,56 @@
-import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm()
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data)
+  })
+
   return (
     <div className='bg-[#ee4d2d]'>
       <div className='max-w-7xl mx-auto px-5'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit}>
               <div className='text-2xl'>Log In</div>
               <div className='mt-8'>
                 <input
                   type='email'
                   name='email'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
-                  placeholder='Phone number'
+                  className='px-3 h-11 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
+                  placeholder='Email'
                 />
-                <div className="mt-1 text-red-600 min-h-[1rem] text-sm"></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
               </div>
               <div className='mt-3'>
                 <input
                   type='password'
                   name='password'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
+                  className='px-3 h-11 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
                   placeholder='Password'
                 />
-                <div className="mt-1 text-red-600 min-h-[1rem] text-sm"></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'></div>
               </div>
-              <div className="mt-3">
-                <button className="w-full rounded text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600">Log In</button>
+              <div className='mt-3'>
+                <button
+                  type='submit'
+                  className='cursor-pointer w-full rounded text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'
+                >
+                  Log In
+                </button>
               </div>
-                <div className="flex items-center justify-center mt-8">
-                  <span className="text-slate-400">New to Shopee?</span>
-                  <Link to="/register" className="text-[#ee4d2d] ml-1 font-bold">Sign Up</Link>
-                </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-slate-400'>New to Shopee?</span>
+                <Link to='/register' className='text-[#ee4d2d] ml-1 font-bold'>
+                  Sign Up
+                </Link>
+              </div>
             </form>
           </div>
         </div>
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,47 +1,73 @@
-import { Link } from "react-router-dom";
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { rules } from '../../utils/rules'
+
+interface FormData {
+  email: string
+  password: string
+  confirm_password: string
+}
 
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormData>()
+
+  const onSubmit = handleSubmit((data) => {
+    // console.log(data)
+  })
+  console.log('error:', errors)
+
   return (
     <div className='bg-[#ee4d2d]'>
       <div className='max-w-7xl mx-auto px-5'>
         <div className='grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10'>
           <div className='lg:col-span-2 lg:col-start-4'>
-            <form className='p-10 rounded bg-white shadow-sm'>
+            <form className='p-10 rounded bg-white shadow-sm' onSubmit={onSubmit} noValidate>
               <div className='text-2xl'>Sign Up</div>
               <div className='mt-8'>
                 <input
                   type='email'
-                  name='email'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
-                  placeholder='Phone Number'
+                  className='px-3 h-11 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
+                  placeholder='Email'
+                  {...register('email', rules.email)}
                 />
-                <div className="mt-1 text-red-600 min-h-[1rem] text-sm"></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>{errors.email?.message}</div>
               </div>
-              <div className='mt-3'>
+              <div className='mt-2'>
                 <input
                   type='password'
-                  name='password'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
+                  className='px-3 h-11 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
                   placeholder='Password'
+                  {...register('password', rules.password)}
                 />
-                <div className="mt-1 text-red-600 min-h-[1rem] text-sm"></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>{errors.password?.message}</div>
               </div>
-              <div className='mt-3'>
+              <div className='mt-2'>
                 <input
                   type='password'
-                  name='confirm_password'
-                  className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
+                  className='px-3 h-11 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm placeholder:text-sm focus:shadow'
                   placeholder='Confirm password'
+                  {...register('confirm_password', rules.confirm_password)}
                 />
-                <div className="mt-1 text-red-600 min-h-[1rem] text-sm"></div>
+                <div className='mt-1 text-red-600 min-h-[1rem] text-sm'>{errors.confirm_password?.message}</div>
               </div>
-              <div className="mt-3">
-                <button className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600">Sign Up</button>
+              <div className='mt-2'>
+                <button
+                  type='submit'
+                  className='cursor-pointer w-full rounded text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-600'
+                >
+                  Sign Up
+                </button>
               </div>
-                <div className="flex items-center justify-center mt-8">
-                  <span className="text-slate-400">Have an account?</span>
-                  <Link to="/login" className="text-[#ee4d2d] ml-1 font-bold">Log In</Link>
-                </div>
+              <div className='flex items-center justify-center mt-8'>
+                <span className='text-slate-400'>Have an account?</span>
+                <Link to='/login' className='text-[#ee4d2d] ml-1 font-bold'>
+                  Log In
+                </Link>
+              </div>
             </form>
           </div>
         </div>
